3c9770b9edf3aaf1d54fc471e187a83e77a0a3f1
KDCLLC <info@kingdavidconsulting.com>
2016-05-12 15:04:41 -0400

    add handlebars for views

--- renderer.js
+++ renderer.js
@@ -1,3 +1,24 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+ 
+    var app = require("./lib/app");
+    
+    app.on("rendered", function(rendered){
+      $("#main").html(rendered);
+    });
+    
+    var showContent = function(view){
+         app.emit("view-selected", view);
+    };
+    
+    $(function(){
+     showContent("home");
+     
+      $(".nav").on("click", function(ev){
+        ev.preventDefault(); //prevent from firin event
+        showContent(this.id);
+      });
+      
+    })
