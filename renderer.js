e7724d5a3ed7a9cc0f4323dcb81f76d6fa815ff6
KDCLLC <info@kingdavidconsulting.com>
2016-05-12 15:21:24 -0400

    add jage engine

--- renderer.js
+++ renderer.js
@@ -2,23 +2,23 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
- 
-    var app = require("./lib/app");
-    
-    app.on("rendered", function(rendered){
-      $("#main").html(rendered);
-    });
-    
-    var showContent = function(view){
-         app.emit("view-selected", view);
-    };
-    
-    $(function(){
-     showContent("home");
-     
-      $(".nav").on("click", function(ev){
+
+var app = require("./lib/app");
+
+app.on("rendered", function (rendered) {
+    $("#main").html(rendered);
+});
+
+var showContent = function (view) {
+    app.emit("view-selected", view);
+};
+
+$(function () {
+    showContent("home");
+
+    $(".nav").on("click", function (ev) {
         ev.preventDefault(); //prevent from firin event
         showContent(this.id);
-      });
-      
-    })
+    });
+
+})
